55688adc1aa11ddd853c97a29c641c077788133a
Vincent Truong <vincenttruong.usa@gmail.com>
2024-12-06 16:04:19 -0500

    Created a Hero Section and added a App.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/Header';
 import About from './components/About';
+import Hero from './components/Hero';
+import './styling/App.css';
 
 function App() {
   return (
-    <div>
+    <div className="app-section">
       <Header />
+      <Hero />
       <About />
     </div>
   );
---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styling/Hero.css';
+
+function Hero() {
+    return(
+        <section className="hero-section" id="Hero">
+            <p>Welcome!</p>
+        </section>
+    )
+}
+
+export default Hero;
